d0554ac3a949fb0f137e5e2a899e2a7b67ac15db
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-01-11 21:48:11 +0100

    add red class, add validator, add endGame, add final phrase

--- js/script.js
+++ js/script.js
@@ -40,6 +40,7 @@ const button = document.getElementById('button');
 const container = document.querySelector('.container');
 const scorePlayer =document.getElementById('score-player');
 
+let isGameOver = false;
 
  
 const startGame = () => {
@@ -82,6 +83,12 @@ const startGame = () => {
         
     }
 
+    const endGame = (score, hasWon = false) => {
+        const message = hasWon ? `Hai vinto!` : `Hai perso! Hai totalizzato ${score} punti.`
+        alert(message);
+        isGameOver = true;
+    }
+
     bombs = generateBombs(totCell);
 
     //numero celle
@@ -93,12 +100,26 @@ const startGame = () => {
     
         // al click mettiamo o togliamo la classe clicked
         cell.addEventListener('click', () => {
-        cell.classList.toggle('clicked');
-        console.log(cell.innerText)
+          if(isGameOver) return;
+          cell.classList.toggle('clicked');
+          console.log(cell.innerText)
 
-        scorePlayer.innerText = ++score;
-        console.log('punteggio:', score)
+           //controllo se ha cliccato una bomba
+           const hasHitBomb = bombs.includes(parseInt(cell.innerText));
 
+           if(hasHitBomb){
+              cell.classList.add('bomb');
+              endGame(score, false)
+           }else{
+             //incremento il punteggio
+             scorePlayer.innerText = ++score;
+
+             //ha vinto?
+             if(score === maxPoints){
+                console.log('hai vinto')
+                endGame(score, true)
+             }
+            }
         })
        
         //aggiungo in pagina la cella
@@ -111,16 +132,5 @@ button.addEventListener('click', startGame)
 
 
 //TODO: COSE DA FINIRE
-/*# MILESTONE 1
-facciamo anche in modo da non poter più cliccare la stessa cella.
 
-
-# MILESTONE 3
-Quando l'utente clicca su una cella, verifichiamo se ha calpestato una bomba, controllando se il numero di cella è presente nell'array di bombe. 
-
-Se si, la cella diventa rossa (raccogliamo il punteggio e scriviamo in console che la partita termina) altrimenti diventa azzurra e dobbiamo incrementare il punteggio.
-# MILESTONE 4
-Quando l'utente clicca su una cella, e questa non è una bomba, dobbiamo controllare se il punteggio incrementato ha raggiunto il punteggio massimo perchè in quel caso la partita termina. Raccogliamo quindi il messaggio è scriviamo un messaggio appropriato.
-(Ma come stabiliamo quale sia il punteggio massimo?)
-# MILESTONE 5
-Quando la partita termina dobbiamo capire se è terminata perchè è stata cliccata una bomba o se perchè l'utente ha raggiunto il punteggio massimo. Dobbiamo poi stampare in pagina il punteggio raggiunto ed il messaggio adeguato in caso di vittoria o sconfitta. */
+// non puoi cliccare due volte nella stessa cella
